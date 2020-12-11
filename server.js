389ad8b6ef26f9f48ccc97217c85d6577cb94704
Paul Nakonieczny <nakonieczny.paul@gmail.com>
2020-12-11 02:34:24 +0000

    Add initial server structure

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const HOST = '0.0.0.0';
+const PORT = 8090;
+
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('css', express.static(__dirname + '/public/css'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    app.send('Hello, World!');
+});
+
+app.listen(PORT, HOST);
